122ef324b59a20151aee1e957bf15a6b7fc63ac6
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-18 00:04:29 +0200

    import Navbar and export to other pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ import {Link}from'react-router-dom';
 function Navbar(){
     return(
         <nav>
-         <Link></Link>
+         <Link to='/'>Home</Link>
+         <Link to='/About'>About</Link>
+          <Link to='/Products'>Products</Link>
+         <Link to='/Contact'>Contact</Link>
+        
         </nav>
     )
 }export default Navbar;
