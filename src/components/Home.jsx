ad75eb46ad242874ebdb0664086972fa4b6b980c
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-18 22:06:43 +0200

    add tailwind to home and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    
+    <div className='flex  justify-center items-center m-4 p-2 gap-4 bg-cover bg-center min-h-screen w-full'
+    style={{backgroundImage:'url(fashion.jpg)'}}>
       
+      {/* Banner or hero Section */}
+      <section className='block text-orange-600 font-bold text-2xl sm:text-2xl md:text-3xl lg:text-5xl rounded-lg  bg-black/40  mt-4 p-6'>
+        <h1>NEW ARRIVALS</h1>
+        <h1>60 % OFF</h1>
+        
+      </section>
+      
+  
     </div>
+    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import {Link}from'react-router-dom';
 
 function Navbar(){
     return(
-        <nav>
+        <nav className='flex flex-col md:flex-row justify-between text-white  bg-orange-500   shadow-lg px-2 py-4 gap-6 m-2 rounded-lg transition-all hover:scale-105 duration-300'>
+            <p className='text-2xl sm:text-2xl md:text-4xl lg:text-5xl'>Winter Fashion</p>
          <Link to='/'>Home</Link>
          <Link to='/About'>About</Link>
           <Link to='/Products'>Products</Link>
